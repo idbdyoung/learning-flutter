3c56e75c2fb918893a920e5e72ec005653260d0f
idbdyoung <idbd.young@gmail.com>
2023-08-01 09:45:39 +0900

    future builder

---
+++ futurebuilder/lib/screen/home_screen.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontSize: 16.0,
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder<int>(
+          future: getNumber(),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasData) {
+
+            }
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'FutureBuilder',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  'ConState: ${snapshot.connectionState}',
+                  style: textStyle,
+                ),
+                Text(
+                  'Data: ${snapshot.data}',
+                  style: textStyle,
+                ),
+                Text(
+                  "Error: ${snapshot.error}",
+                  style: textStyle,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text('setState'),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(Duration(seconds: 3));
+
+    final random = Random();
+
+    return random.nextInt(100);
+  }
+}
